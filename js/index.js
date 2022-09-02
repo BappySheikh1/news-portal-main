b9f1b3c58fc9f72e5d665fabd9dd810c0f72a3e8
Your Name Main <you@example.com>
2022-09-02 11:59:08 -0700

    group card added

--- js/index.js
+++ js/index.js
@@ -1,38 +1,3 @@
-// const newsCategory=()=>{
-//     fetch(`https://openapi.programming-hero.com/api/news/category/04`)
-//     .then(res=> res.json())
-//     .then(data=>displayNewsCategory(data.data))
-    
-// }
-// const displayNewsCategory=datas=>{
-//     console.log(datas)
-//     const mainContainer=document.getElementById('main-container')
-//   datas.forEach(data => {
-//     // console.log(data)
-//     const mainDiv=document.createElement('div')
-//     mainDiv.classList.add('col')
-//     mainDiv.innerHTML=`
-//     <div class="card h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
-//                 <img src="${data.thumbnail_url}" class="card-img-top" alt="...">
-//                 <div class="card-body">
-//                   <h5 class="card-title">${data.title}</h5>
-//                   <p class="card-text">${data.details.slice(0, 300)}</p>
-//                   <div class="d-flex justify-content-between align-items-center">
-//                   <div class="d-flex align-items-center">
-//                   <img src="${data.author.img}" class="img-fluid w-25 h-25 rounded-circle">
-//                   <h5>${data.author.name? data.author.name:'There is no data '}</h5>
-//                   </div>
-//                     <h5>${data.total_view? data.total_view:'There is no data'}</h5>
-//                   </div>
-
-//                 </div>
-//               </div>
-  
-//     `
-//     mainContainer.appendChild(mainDiv)
-//   });
-// }
-// newsCategory()
 
 const categoryData=()=>{
     fetch(`https://openapi.programming-hero.com/api/news/categories`)
@@ -42,23 +7,50 @@ const categoryData=()=>{
 }
 const displayCategory=datas=>{
     const categoriesContainer=document.getElementById('category-container')
+    categoriesContainer.innerText='';
     datas.forEach(data => {
         const categoriesDiv=document.createElement('span')
-        console.log(data)
+        // console.log(data)
         categoriesDiv.innerHTML=`
 
-        <p onclick="displayCategoryId(${data.category_id})">${data.category_name}</p>
+        <p onclick="dataCategoryId('${data.category_id}')">${data.category_name}</p>
        
         `
         categoriesContainer.appendChild(categoriesDiv)
     });
 
 }
-const dataCategoryId=categoryId=>{
- const url=(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
+const dataCategoryId=(id)=>{
+ const url=(`https://openapi.programming-hero.com/api/news/category/${id}`)
  fetch(url)
  .then(res=> res.json())
- .then(data=>console.log(data.data))
+ .then(data=>dataDisplay(data.data))
 }
-
+const dataDisplay=datas=>{
+    const mainContainer=document.getElementById('main-container')
+    mainContainer.innerText=''
+datas.forEach(data=>{
+    console.log(data)
+    const mainDiv=document.createElement('div')
+    mainDiv.classList.add('col')
+    mainDiv.innerHTML=`
+    <div class="card h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
+       <img src="${data.thumbnail_url}" class="card-img-top" alt="...">
+       <div class="card-body">
+         <h5 class="card-title">${data.title}</h5>
+           <p class="card-text">${data.details.slice(0, 300)}</p>
+           <div class="d-flex justify-content-between align-items-center">
+           <div class="d-flex align-items-center">
+           <img src="${data.author.img}" class="img-fluid w-25 h-25 rounded-circle">
+           <h5>${data.author.name? data.author.name:'There is no data '}</h5>
+             </div>
+            <h5>${data.total_view? data.total_view:'There is no data'}</h5>
+          </div>
+    
+         </div>
+      </div>
+    `
+    mainContainer.appendChild(mainDiv)
+})
+}
 categoryData()
