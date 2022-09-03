7374fe95008325bdc04bb852d4b88f1f295bd47b
Your Name Main <you@example.com>
2022-09-02 19:01:42 -0700

    modal added

--- js/index.js
+++ js/index.js
@@ -9,7 +9,8 @@ const displayCategory=datas=>{
     const categoriesContainer=document.getElementById('category-container')
     categoriesContainer.innerText='';
     datas.forEach(data => {
-        const categoriesDiv=document.createElement('span')
+      toggleSpinner(true)
+        const categoriesDiv=document.createElement('div')
         // console.log(data)
         categoriesDiv.innerHTML=`
 
@@ -39,11 +40,11 @@ const dataDisplay=datas=>{
     }
 
 datas.forEach(data=>{
-    console.log(data)
+    // console.log(data)
     const mainDiv=document.createElement('div')
     mainDiv.classList.add('col')
     mainDiv.innerHTML=`
-    <div class="card h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <div class="card h-100" >
        <img src="${data.thumbnail_url}" class="card-img-top" alt="...">
        <div class="card-body">
          <h5 class="card-title">${data.title}</h5>
@@ -55,11 +56,53 @@ datas.forEach(data=>{
              </div>
             <h5>${data.total_view? data.total_view:'There is no data'}</h5>
           </div>
-    
          </div>
+         <button onclick="detailData('${data._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-primary w-25 text-center">Detail</button>
       </div>
     `
     mainContainer.appendChild(mainDiv)
 })
+toggleSpinner(false)
+}
+
+// Spinner added
+const toggleSpinner=isLoader=>{
+  const loaderSpinner=document.getElementById('loader')
+  if(isLoader === true){
+    loaderSpinner.classList.remove('d-none')
+  }
+  else{
+    loaderSpinner.classList.add('d-none')
+  }
+}
+// Modal Detail Data
+const detailData=newId=>{
+  const url=`https://openapi.programming-hero.com/api/news/${newId}`
+  fetch(url)
+  .then(res=> res.json())
+  .then(data=> displayDetailData(data.data))
+  .catch(error=>console.log(error))
+}
+const displayDetailData=datas=>{
+  datas.forEach(data => {
+    console.log(data)
+    const modalTitle=document.getElementById('exampleModalLabel')
+    modalTitle.innerText=data.title
+    const modalBodyContainer=document.getElementById('modalBody-container')
+    modalBodyContainer.innerHTML=`
+    <img src="${data.image_url}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Publish Date: ${data.author.published_date? data.author.published_date:'No published date'}</h5>
+        <p class="card-text">${data.details}</p>
+        <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center">
+        <img src="${data.author.img}" class="img-fluid w-25 h-25 rounded-circle">
+        <h5>${data.author.name? data.author.name:'There is no data '}</h5>
+          </div>
+         <h5>${data.total_view? data.total_view:'There is no data'}</h5>
+       </div>
+      </div>
+    `
+  });
 }
 categoryData()
